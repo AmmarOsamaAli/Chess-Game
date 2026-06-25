fe9b0a286cf20f066187b98f0de1de3191f5ef69
Ammar Osama <ammarosama080@icloud.com>
2026-06-25 14:12:48 +0300

    Cleaned the Code from unnecessary lines

--- get-started/get-started.js
+++ get-started/get-started.js
@@ -1,5 +1,3 @@
-console.log('Lets Get Started')
-
 /*---------------------------Cached Elemetns--------------------------------*/
 
 // Authentication
@@ -12,42 +10,10 @@ const ChoosenTimeControl = document.querySelector('#choosen-time-control')
 const childDiv = ChoosenTimeControl.querySelector('child-div-of-choosen-timer-btn')
 const dropdownArrow = document.querySelector('#dropdown-arrow')
 const timerForGame = document.querySelectorAll('#timer-for-game')
-const bulletTimer = document.querySelector('#bullet')
-const blitzTimer = document.querySelector('#blitz')
-const rapidTimer = document.querySelector('#rapid')
-
 
 // Start Game
 const StartGameBtn = document.querySelector('#start-game')
 
-
-console.log(logInBtn)
-console.log(signUpBtn)
-console.log(actualDropdown)
-console.log(ChoosenTimeControl)
-console.log(dropdownArrow)
-console.log(timerForGame)
-console.log(bulletTimer)
-console.log(blitzTimer)
-console.log(rapidTimer)
-console.log(StartGameBtn)
-console.log(logInBtn)
-console.log(signUpBtn)
-
-
-/*---------------------------Constant--------------------------------*/
-
-const allTimerControl = [
-    { timer: '1 min', type: 'Bullet', emoji: '🚅' },
-    { timer: '1 + 1', type: 'Bullet', emoji: '🚅' },
-    { timer: '2 + 1', type: 'Bullet', emoji: '🚅' },
-    { timer: '3 min', type: 'Blitz', emoji: '⚡' },
-    { timer: '3 + 2', type: 'Blitz', emoji: '⚡' },
-    { timer: '5 min', type: 'Blitz', emoji: '⚡' },
-    { timer: '10 min', type: 'Rapid', emoji: '⏱️' },
-    { timer: '10 + 5', type: 'Rapid', emoji: '⏱️' },
-    { timer: '15 + 10', type: 'Rapid', emoji: '⏱️' },
-]
 
 /*---------------------------Functions--------------------------------*/
 
@@ -85,7 +51,6 @@ function DropdownFunctionality(event) {
         if (actualDropdown.style.display = 'flex')
             actualDropdown.style.display = 'none'
         dropdownArrow.textContent = '▼'
-        console.log('Dropdown')
     }
 }
 
